53cec87d699f46b820cf452a959e73b8af9765b7
loganathanramsaran <loguflower3@gmail.com>
2025-03-11 17:44:52 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
-import { Header } from './components/header'
+// import { Header } from './components/Header'
 import { Home } from './components/Home'
 import { Cart } from './components/Cart'
 import { useState } from 'react'
