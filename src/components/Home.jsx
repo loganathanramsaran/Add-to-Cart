8df3550a7d2acadf0cf5892f28f4dd9b325adce7
loganathanramsaran <loguflower3@gmail.com>
2025-03-11 16:36:40 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 import data from '../assets/Products.json'
 import { Product } from './Product'
 
-export const Home = () => {
+export const Home = ({cart, setCart}) => {
     const [products] = useState(data)
   return (
     <div className='product-container'>
+      <div class="w-[100%]">
+            <h1 class="pt-8 text-4xl text-center bg-[#e7dbef]"><span class="text-purple-950">Welcome</span> to Shop Cart </h1>
+            <p class="pt-1 text-sm pb-8 text-gray-600 text-center bg-[#e7dbef]">Make it Perfect, <span class="text-gray-800">Choice is Yours!</span></p>
+      </div>
         {products.map((product)=>(
             <Product key={product.id} product=
-            {product}/>
+            {product} cart={cart} setCart={setCart}/>
         ))}
         </div>
   )
