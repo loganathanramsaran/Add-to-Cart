d3786b01333bf7df5e3c361baa4e8d5c92144939
loganathanramsaran <loguflower3@gmail.com>
2025-03-23 01:50:13 +0530

    api fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from 'react'
-import data from '../assets/Products.json'
-import { Product } from './Product'
+import React, { useState } from "react";
+import { Product } from "./Product";
 
-export const Home = ({cart, setCart}) => {
-    const [products] = useState(data)
+export const Home = ({ cart, setCart }) => {
+  const [products, setProducts] = useState([]);
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => setProducts(json));
+
   return (
-    <div className='product-container'>
-      <div class="w-[100%]">
-            <h1 class="pt-8 text-4xl text-center bg-[#e7dbef]"><span class="text-purple-950">Welcome</span> to Shop Cart </h1>
-            <p class="pt-1 text-sm pb-8 text-gray-600 text-center bg-[#e7dbef]">Make it Perfect, <span class="text-gray-800">Choice is Yours!</span></p>
+    <div className="product-container">
+      <div className="w-screen">
+        <h1 className="pt-8 text-4xl text-center bg-[#e7dbef]">
+          <span className="text-purple-950">Welcome</span> to Shop Cart{" "}
+        </h1>
+        <p className="pt-1 text-sm pb-8 text-gray-600 text-center bg-[#e7dbef]">
+          Make it Perfect,{" "}
+          <span className="text-gray-800">Choice is Yours!</span>
+        </p>
       </div>
-        {products.map((product)=>(
-            <Product key={product.id} product=
-            {product} cart={cart} setCart={setCart}/>
-        ))}
-        </div>
-  )
-}
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          product={product}
+          cart={cart}
+          setCart={setCart}
+        />
+      ))}
+    </div>
+  );
+};
